e567bd710a4e61c3475f14d8cc63f825c5c0d7c8
GeritoJavita <geronimo.garciaalarcon@gmail.com>
2026-07-16 17:54:35 -0500

    Desarrollando paso a paso del trabajo como desarrolladores de software}

---
+++ components/Process.tsx
@@ -0,0 +1,18 @@
+import {
+Search,
+PenTool,
+Code2,
+Racket,
+} from "lucide-react";
+
+const steps = [
+    {
+        number: "01",
+        title: "Discovery",
+        description:
+        "We understand your business, goals adn project requirements to define the best technical solution.",
+        icon: Search,
+
+    },
+    
+]
